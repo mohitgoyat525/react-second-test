3d40d7ba710d1bbe19b3d749d44607baa36e9fb4
mohitgoyat525 <montyits71@gmail.com>
2024-12-30 15:12:27 +0530

    add-changes

--- src/components/Competition.jsx
+++ src/components/Competition.jsx
@@ -13,33 +13,36 @@ const Competition = () => {
                       <p className='custom-text text-white custom-max-w'>The current return and refund process centres around either traditional advice or software. By combining both together for an aggressively affordable fee, we think we can capture much of the demand for those people that feel competent in managing their own refunds / returns.</p>
                       <p className='custom-text text-white custom-max-w'>As well as being the cheapest in market, our product offers everything a sole trader needs in one place, whilst pricing innovation of zero commission rather than a commission, will drive customers that are particularly price sensitive.</p>
                   </div>
-                  <div className='col-12 col-lg-6 table-bg d-flex border-radius-xl'>
-                      <div className='d-flex align-items-center justify-content-center'>
-                        
-                          <table className='custom-table-bg'>
-                              <thead>
-                                  <tr>
-                                      <th></th>
-                                      <th className='custom-rounded-top-left-border text-center bg-white fw-semibold table-heading '>Rift</th>
-                                      <th className='custom-rounded-top-left-border text-center bg-white fw-semibold table-heading '>Taxscout</th>
-                                      <th className='custom-rounded-top-left-border text-center bg-white fw-semibold table-heading '>Coconut</th>
-                                      <th className='custom-rounded-top-left-border  text-center bg-lightGreen fw-semibold table-heading'>Pie</th>
-                                  </tr>
-                              </thead>
-                              <tbody>
-                                  {COMPETITION_LIST.map((obj, i) => (
-                                      <tr key={i}>
-                                          <td className={`text-white fw-normal custom-td bg-transparent custom-border-b-none`}>{obj.brand}</td>
-                                          <td className={`text-center ${i===5 ? 'custom-fee custom-rounded-top-left-border-bottom':''} bg-white custom-td custom-border-b-none`}>{obj.rift}</td>
-                                          <td className={`text-center ${i===5 ? 'custom-fee custom-rounded-top-left-border-bottom':''} bg-white custom-td custom-border-b-none`}>{obj.taxscout}</td>
-                                          <td className={`text-center ${i===5 ? 'custom-fee custom-rounded-top-left-border-bottom':''} bg-white custom-td custom-border-b-none`}>{obj.coconut}</td>
-                                          <td className={`text-center bg-lightGreen custom-td custom-border-b-none ${i === 5 ? 'custom-fee custom-rounded-top-left-border-bottom' : ''}`}>{obj.pie}</td>
+                  <div className="col-12 col-lg-6 table-bg d-flex border-radius-xl align-items-center justify-content-center sm-py-xl overflow-hidden">
+                      <div className="d-flex align-items-center justify-content-center w-100">
+                          <div className="table-responsive"> 
+                              <table className="custom-table-bg">
+                                  <thead>
+                                      <tr>
+                                          <th class=" d-sm-table-cell"></th>
+                                          <th className="custom-rounded-top-left-border text-center bg-white fw-semibold table-heading">Rift</th>
+                                          <th className="custom-rounded-top-left-border text-center bg-white fw-semibold table-heading">Taxscout</th>
+                                          <th className="custom-rounded-top-left-border text-center bg-white fw-semibold table-heading">Coconut</th>
+                                          <th className="custom-rounded-top-left-border text-center bg-lightGreen fw-semibold table-heading">Pie</th>
                                       </tr>
-                                  ))}
-                              </tbody>
-                          </table>
+                                  </thead>
+                                  <tbody>
+                                      {COMPETITION_LIST.map((obj, i) => (
+                                          <tr key={i}>
+                                              <td className={`text-white fw-normal custom-td bg-transparent custom-border-b-none ${i === 3 ? 'max-w-sm' : ''}`}>{obj.brand}</td>
+                                              <td className={`text-center ${i === 5 ? 'custom-fee custom-rounded-top-left-border-bottom' : ''} bg-white custom-td custom-border-b-none`}>{obj.rift}</td>
+                                              <td className={`text-center ${i === 5 ? 'custom-fee custom-rounded-top-left-border-bottom' : ''} bg-white custom-td custom-border-b-none`}>{obj.taxscout}</td>
+                                              <td className={`text-center ${i === 5 ? 'custom-fee custom-rounded-top-left-border-bottom' : ''} bg-white custom-td custom-border-b-none`}>{obj.coconut}</td>
+                                              <td className={`text-center bg-lightGreen custom-td custom-border-b-none ${i === 5 ? 'custom-fee custom-rounded-top-left-border-bottom' : ''}`}>{obj.pie}</td>
+                                          </tr>
+                                      ))}
+                                  </tbody>
+                              </table>
+                          </div>
                       </div>
                   </div>
+
+
 
 
               </div>
